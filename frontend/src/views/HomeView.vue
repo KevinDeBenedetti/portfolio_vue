1a705b848ec3d36d1a601f7a86544927e2c802b8
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2023-09-24 19:14:39 +0200

    Fix frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,104 +2,14 @@
 import SideLock from "@/components/SideLock.vue";
 import Realisations from "@/components/RealisationsSection.vue";
 import Skills from "@/components/SkillsSection.vue";
-import {onMounted} from "vue";
-
-/* Code for sticky header */
-/*window.addEventListener('scroll', function() {
-
-  const sections = document.querySelectorAll('.section');
-
-  const headerTitle = document.getElementById('header-title');
-
-  let currentSection = '';
-
-  sections.forEach(function(section) {
-
-    const sectionTop = section.offsetTop;
-
-    console.log(section)
-    console.log('SectionTop :' + sectionTop)
-    console.log('PageYOffset :' + window.pageYOffset)
-
-    const sectionHeight = section.offsetHeight;
-    console.log('height :' + sectionHeight)
-
-/!*    if (window.pageYOffset >= sectionTop && window.pageYOffset < sectionTop + sectionHeight) {
-      currentSection = section.querySelector('.section-title').textContent;
-    }*!/
-
-    if (window.pageYOffset >= sectionTop && window.pageYOffset < sectionTop + sectionHeight) {
-      currentSection = section.querySelector('.section-title').innerText;
-
-      console.log("Afficher le titre de la section");
-      console.log(currentSection);
-    }
-
-  });
-
-
-  // Si currentSection reste vide, utilisez un titre par défaut
-  if (currentSection === '') {
-    currentSection = 'Default title';
-  }
-
-  headerTitle.textContent = currentSection;
-});*/
-
-/*onMounted(() => {
-  const stickyHeader = document.querySelector('#header-title');
-
-  const sections = document.querySelectorAll('.section');
-
-// Options pour l'Intersection Observer
-  const options = {
-    root: null, // viewport
-    threshold: 0.2, // pour déclencher l'événement immédiatement
-    // rootMargin: "2% 0%" // pour déclencher l'événement 200px avant
-  };
-
-  let currentSection = null;
-
-  const callback = entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const title = entry.target.querySelector('h3').innerText;
-        stickyHeader.innerText = title;
-        console.log(entry.target)
-        currentSection = entry.target;
-      } else if (entry.target === currentSection) {
-        currentSection = null;
-      }
-    });
-  };
-
-  // Créer l'Intersection Observer
-  const observer = new IntersectionObserver(callback, options);
-
-  console.log(stickyHeader);
-
-  console.log(sections);
-
-// Observer toutes les sections
-  sections.forEach(section => {
-    observer.observe(section);
-  });
-})*/
-
 
 </script>
 
 <template>
 
-<!--  <div class="sticky-header">
-
-    <h2 id="header-title">Default title</h2>
-
-  </div>-->
-
   <main class="lg:flex h-screen">
 
-    <aside class="p-12 text-white lg:w-4/12 flex flex-col justify-between">
+    <aside class="p-12 text-white lg:w-4/12 flex flex-col justify-around">
 
       <SideLock/>
 
@@ -107,9 +17,9 @@ import {onMounted} from "vue";
 
     <div class="text-white overflow-auto lg:w-8/12">
 
-      <Realisations class="section"/>
+      <Realisations />
 
-      <Skills class="section"/>
+      <Skills />
 
     </div>
 
@@ -118,11 +28,5 @@ import {onMounted} from "vue";
 </template>
 
 <style scoped>
-.sticky-header {
-  position: sticky;
-  top: 0;
-  background-color: #f1f1f1;
-  padding: 10px;
-  z-index: 10;
-}
+
 </style>
--- frontend/src/views/SymfonyView.vue
+++ frontend/src/views/SymfonyView.vue
@@ -4,30 +4,6 @@
 
 <template>
 
-<!--  <SideNav v-if="currentSubCategory !== null" :documentationTitle="'Vue JS - ' +currentSubCategory.subCategoryTitle">
-
-    <template v-slot:default>
-
-      <Content :items="currentSubCategory.items"/>
-
-    </template>
-
-  </SideNav>
-
-  <SideNav v-else documentationTitle="Bienvenue dans ma documentation de Vue V3">
-    <template v-slot:default>
-
-      <div class="container xl:max-w-5xl py-20 px-5 m-auto">
-
-        <div>
-          <h2>Bienvenue dans ma documentation sur Symfony</h2>
-        </div>
-
-      </div>
-
-    </template>
-  </SideNav>-->
-
 </template>
 
 <style scoped>
